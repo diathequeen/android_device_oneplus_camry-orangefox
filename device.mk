646dd96595170a2ae8be4feb0007cf59365a1696
DiamondQueen <ghostieqwe@gmail.com>
2026-09-16 13:37:45 +0300

    camry: cleanup v1: remove bootctrl packages

--- device.mk
+++ device.mk
@@ -20,10 +20,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Configure twrp common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_PACKAGES += \
-    bootctrl.oneplus_sm6375.recovery \
-    android.hardware.boot@1.2-impl-qti.recovery
-
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
